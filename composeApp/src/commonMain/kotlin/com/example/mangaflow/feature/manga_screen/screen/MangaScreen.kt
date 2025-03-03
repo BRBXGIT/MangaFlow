418c8d794540b54cde40ceb0a0c8744c878357d8
BRBX <egorka2005sautkin@gmail.com>
2025-03-03 16:43:17 +0300

    want to rewrite loading animtion

--- composeApp/src/commonMain/kotlin/com/example/mangaflow/feature/manga_screen/screen/MangaScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/mangaflow/feature/manga_screen/screen/MangaScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
